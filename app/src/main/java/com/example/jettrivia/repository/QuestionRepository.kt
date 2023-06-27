82128af7aed47e8281272288fa21fbdcde10ef5a
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-06-27 15:12:35 +0530

    Creating a wrapper class for emitting metadata

---
+++ app/src/main/java/com/example/jettrivia/repository/QuestionRepository.kt
@@ -0,0 +1,20 @@
+package com.example.jettrivia.repository
+
+import android.media.session.MediaSession.QueueItem
+import com.example.jettrivia.data.DataOrException
+import com.example.jettrivia.model.QuestionItem
+import com.example.jettrivia.network.QuestionApi
+import javax.inject.Inject
+
+class QuestionRepository@Inject constructor(
+    private val api: QuestionApi
+){
+    private val listofQuestions =
+        DataOrException<ArrayList<QuestionItem>,
+                Boolean,
+                Exception>()
+
+
+
+
+}
