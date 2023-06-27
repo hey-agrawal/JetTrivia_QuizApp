72710678a728286635844c8bea20bd4f5ac4d349
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-06-27 15:28:41 +0530

    Repository Class

--- app/src/main/java/com/example/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/jettrivia/repository/QuestionRepository.kt
@@ -1,6 +1,8 @@
 package com.example.jettrivia.repository
 
 import android.media.session.MediaSession.QueueItem
+import android.util.Log
+//import androidx.compose.ui.tooling.data.EmptyGroup.data
 import com.example.jettrivia.data.DataOrException
 import com.example.jettrivia.model.QuestionItem
 import com.example.jettrivia.network.QuestionApi
@@ -8,13 +10,26 @@ import javax.inject.Inject
 
 class QuestionRepository@Inject constructor(
     private val api: QuestionApi
-){
-    private val listofQuestions =
+) {
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>,
                 Boolean,
                 Exception>()
 
+    suspend fun getAllQuestion(): DataOrException<ArrayList<QuestionItem>, Boolean, java.lang.Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
 
-
-
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestion: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
+    }
 }
+
+
+
+
